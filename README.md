de12423d2bab74eacee84333d959f25903b4c939
Levi Crouch <levi.crouch@gmail.com>
2017-12-05 17:01:42 -0800

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Giphy-API
+
+# Overview:
+
+Leverages the giphy API to dynamically download images based pre-defined and user defined queries.
---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// pseudo code
+// page loads with a few pre-defined buttons labelled with animal names and an input box
+// when a user clicks the predefined buttons, the page shows several pictures of the animal selected
+// user eneters a new animal into the search box and a new button is created and the newly searched for animal is displayed below
+// use ajax to query the giphy API
+// use "document" to to listen for click events
+// use prevent default to prevent the submit button for behaving weird
+document.click(); 
