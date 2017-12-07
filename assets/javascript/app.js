4b16475e545f1a2dcf62b698a972fb4f55038c93
Levi Crouch <levi.crouch@gmail.com>
2017-12-07 00:20:53 -0800

    some code cleanup and more styling tweaks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,11 @@
 $(document).ready(function () {
-    // pseudo code
-
-    // when a user clicks the predefined buttons, the page shows several pictures of the animal selected
-    // user enters a new animal into the search box, a new button is created
-    // display 10 images
-    // images are static until you click them to animate. 
-    // Click again to stop the animation
-    // display rating for each image
-    // use ajax to query the giphy API
-    // use "document" to to listen for click events
-    // use prevent default to prevent the submit button for behaving weird
-
     ///////////////////////////////////////////////////////
     // objects and variables
     ///////////////////////////////////////////////////////
     // giphy API key
     var apiKey = "q6hBQIsve5u8m6jAO8phsbX7SMEqcXDv";
     var buttonClass = ".button-landing";
-    var imageClass = "#image-row";
+    var imageRowID = "#image-row";
     var arrDefaultButtons = ["kittens", "puppies", "hamsters", "sloth", "goats", "mini+pigs", "pandas", "hedgehog", "gecko", "otters", "beavers"];
     var maxNumberOfImages = 10;
     var inputID = "#add-query";
@@ -29,65 +17,57 @@ $(document).ready(function () {
 
     function getImgData() {
         // capture search data from button:
-        $(imageClass).empty();
+        $(imageRowID).empty();
         var query = $(this).attr("data-query");
-        console.log("query:", query);
-        // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=5";
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + maxNumberOfImages;
         var xhr = $.get(queryURL);
         xhr.done(function (data) {
-            console.log("success got data", data);
-            // create object for giphy data
-            // var giphyDataArray = data.data;
             // iterate through array and pull out the image URL
             for (i = 0; i < data.data.length; i++) {
-                // grab url information of gif
-                // var imgURL = giphyDataArray[i].images.downsized.url;
+                // grab url information of static gif
                 var staticImgURL = data.data[i].images.fixed_height_still.url;
+                // grab animated gif url
                 var animatedImgURL = data.data[i].images.fixed_height.url;
+                // grab the title, rating, and giphy url of the gif
                 var gifTitle = data.data[i].title;
                 var gifRating = data.data[i].rating;
-                var giphyLink = data.data[i].bitly_url;                
-                console.log("giphyLink:", giphyLink);
+                var giphyLink = data.data[i].bitly_url;
 
-                // Add a rating for the image
-                // add a thumbnail div to contain the image
+                // add a column div to contain the thumbnail div
                 var columnDiv = $("<div>");
                 columnDiv.addClass("col-sm-6 col-md-4 images");
-
-                // Add a rating for the image
-                // add a thumbnail div to contain the image
+                // add a thumbnail div to contain the image, caption and rating
                 var thumbnailDiv = $("<div>");
                 thumbnailDiv.addClass("thumbnail");
-                
+
                 // place image in html document
                 var img = $("<img>");
-                // img.addClass("downloaded-images");
+                // set the img src to the static image
                 img.attr("src", staticImgURL);
+                // set the class property
                 img.addClass("downloaded-image img-responsive");
                 img.attr("alt", gifTitle);
+                // set the data-static property to the static url
                 img.attr("data-static", staticImgURL);
+                // Set the data-animate property to the animated image url
                 img.attr("data-animate", animatedImgURL);
+                // Set the default state to static
                 img.attr("data-state", "static");
-                // img.attr("class", "img-responsive");
+                // add the image div into the thumbnail div
                 $(thumbnailDiv).append(img);
-                
-
                 // add a caption div to contain the image
                 var captionDiv = $("<div>");
                 captionDiv.addClass("caption");
-                
-
                 // add the title of the image to the caption div heading
                 $(captionDiv).html("<h3>" + gifTitle + "</h3>");
                 // add the rating of the image to the caption div heading
                 $(captionDiv).append("<p>Rating: " + gifRating + "</p>");
-                          
-                $("#image-row").append(columnDiv);
+                // add the image row, column div, and caption div to the thumbnail div
+                $(imageRowID).append(columnDiv);
                 $(columnDiv).append(thumbnailDiv);
                 $(thumbnailDiv).append(captionDiv);
-                
-                
+
+
             }
 
         });
@@ -96,38 +76,33 @@ $(document).ready(function () {
 
     function renderButtons() {
         // verify that we are in the button div
-        console.log("Start of renderButtons");
-        // Deleting the buttons prior to adding new movies
+        // Deleting the buttons prior to adding new buttons
         $(buttonClass).empty();
         // page loads with a few pre-defined buttons labelled with animal names and an input box
-        // render the default buttons
-        // <button type="button" class="btn btn-primary">Primary</button>
+        // render the default buttons populated with the values from the arrDefaultButtons
         for (i = 0; i < arrDefaultButtons.length; i++) {
             var b = $("<button>");
             b.addClass("btn btn-primary default-buttons");
-            // b.addClass("default-buttons");
             b.attr("data-query", arrDefaultButtons[i]);
             b.text(arrDefaultButtons[i]);
             $(buttonClass).append(b);
         }
     }
 
-    // This function handles events where the add movie button is clicked
+    // This function handles events where the add inputID button is clicked
     $(inputID).on("click", function (event) {
         event.preventDefault();
         // This line of code will grab the input from the textbox
         var query = $(inputData).val().trim();
-        console.log("query:", query);
-
-        // The movie from the textbox is then added to our array
+       // The query term from the textbox is then added to our array
         arrDefaultButtons.push(query);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of our query array
         renderButtons();
     });
-    // Function for displaying the movie info
-    // Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
-    $(document).on("click", ".default-buttons", imageClass, getImgData);
+    // Function for displaying the default query info
+    // Using $(document).on to capture click events
+    $(document).on("click", ".default-buttons", imageRowID, getImgData);
 
     // Calling the renderButtons function to display the initial buttons
     renderButtons();
